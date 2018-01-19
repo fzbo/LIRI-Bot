201cb5a0f7e5a8b4483d9aa71c04ac76c0463c6a
fzbo <elizabeth.bazan@gmail.com>
2018-01-19 18:50:47 -0500

    Second update to include Twitter API Keys

--- keys.js
+++ keys.js
@@ -2,8 +2,9 @@ console.log('this is loaded');
 
 exports.twitterKeys = {
 
-	consumer_key: '<input here>',
-	consumer_secret: '<input here>',
-	access_token_key: '<input here>',
+	consumer_key: 'wjxMhqDfjKgy3NxkuQFlSuV5D',
+	consumer_secret: '4OKOvkG0PZWO2Vn7HkLw10fBgzS0wjO5bs8qAOfRLCiWAJcDh5',
+	access_token_key: '950139735766392832-6r4ffzUACyOjcA4NZI1RKoHHuLqsXg0',
+	access_token_secret: '	0riwC4rfEVwi8SUvXAh76Uy3hX15y3iPlFGd9xRWOzdlJ',
 	
 }
