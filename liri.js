766bb7c1215d3531a55948aeead6b4a9bcb917da
fzbo <elizabeth.bazan@gmail.com>
2018-02-12 23:15:33 -0500

    Update added corrected node spotify package

--- liri.js
+++ liri.js
@@ -1,6 +1,22 @@
 //KEY SOURCES FILE
 var keys = require("./keys.js");
 
+// DEPENDENCIES
+// =====================================
+// Import the Keys file
+var keys = require("./keys.js");
+
+// Import the Twitter NPM package.
+var Twitter = require("twitter");
+
+// Import the node-spotify-api NPM package.
+var Spotify = require("node-spotify-api");
+
+// Import the request npm package.
+var request = require("request");
+//KEY SOURCES FILE
+var keys = require("./keys.js");
+
 //DEPENDENCIES
 var Twitter = require("twitter");
 var Spotify = require("spotify");
@@ -13,18 +29,18 @@ var arg = process.argv[2];
 
 
 var myTweets = function() {
-	var client = new Twitter(keys.twitterKeys);
+  var client = new Twitter(keys.twitterKeys);
 
-	var params = {screen_name: 'pepperpawz'};
-	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  var params = {screen_name: 'pepperpawz'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-  		for (var i = 0; i < tweets.length; i++) {
-  			console.log(tweets[i].created_at);
-  			console.log("");
-  			console.log(tweets[i].text);
-  		}
-  	}
-	
+      for (var i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].created_at);
+        console.log("");
+        console.log(tweets[i].text);
+      }
+    }
+  
   });
 
 };
